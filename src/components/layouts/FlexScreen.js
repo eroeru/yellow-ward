2c0bb59f6731258effa206e35c53f6811d5f0507
eroeru <eero.elvisto@gmail.com>
2021-08-08 16:48:07 +0300

    Updates

--- src/components/layouts/FlexScreen.js
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components";
-
-export default styled.div`
-  display: flex;
-  font-size: 4rem;
-  width: 100vw;
-  height: 100vh;
-`;
--- src/components/styles/colors.js
+++
@@ -1,7 +0,0 @@
-export const colors = {
-    primary_yellow: `#F7E033;`,
-    primary_grey: '#3f3f3f',
-    secondary_yellow: 'rgba(247, 224, 51, 0.4)',
-    secondary_grey: '#F2F2F2',
-  };
-  
