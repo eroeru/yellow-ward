e8e93bb105b9a29e7b88fac56d50ae693ca88456
eroeru <eero.elvisto@gmail.com>
2021-08-08 17:05:02 +0300

    Updates

--- src/tests/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from '../components/App';
-
-test('renders ward', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/ward/i);
-  expect(linkElement).toBeInTheDocument();
-});
